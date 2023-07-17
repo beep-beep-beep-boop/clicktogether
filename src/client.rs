2e6728ba7e786ceedf8c1f737d5a159e9c33332e
beep-beep-beep-boop <79901832+beep-beep-beep-boop@users.noreply.github.com>
2023-07-17 09:17:54 -0600

    add error handling to client

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,7 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode},
 };
 use futures::StreamExt;
+use reqwest::StatusCode;
 
 pub async fn start_client(
     address: String,
@@ -10,14 +11,19 @@ pub async fn start_client(
 ) -> Result<(), Box<dyn std::error::Error>> {
     println!("connecting to server...");
 
-    send_join(&address, &username).await?;
+    match send_join(&address, &username).await {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("! could not connect to server >< ! {}", err);
+            std::process::exit(1);
+        }
+    };
 
-    println!("connected! :3")
+    println!("connected! :3");
     println!("press escape to exit!!");
     println!("press space to send a click!!");
 
     enable_raw_mode()?;
-
 
     let mut reader = EventStream::new();
 
@@ -28,7 +34,12 @@ pub async fn start_client(
             Some(Ok(event)) => {
                 if event == Event::Key(KeyCode::Char(' ').into()) {
                     println!("sending click!\r");
-                    send_click(&address, &username).await?;
+                    match send_click(&address, &username).await {
+                        Ok(_) => {}
+                        Err(err) => {
+                            eprintln!("! could not send click to server >< ! {}\r", err);
+                        }
+                    };
                 }
 
                 if event == Event::Key(KeyCode::Esc.into()) {
@@ -44,7 +55,15 @@ pub async fn start_client(
 
     println!("exiting...");
 
-    send_leave(&address, &username).await?;
+    match send_leave(&address, &username).await {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!(
+                "! could not tell the server we were leaving >< ! is it down? {}",
+                err
+            );
+        }
+    };
 
     Ok(())
 }
@@ -52,7 +71,11 @@ pub async fn start_client(
 async fn send_join(address: &String, username: &String) -> Result<(), Box<dyn std::error::Error>> {
     let url = format!("{}/join/{}", address, username);
     let client = reqwest::Client::new();
-    client.put(url).send().await?;
+    let res = client.put(url).send().await?;
+
+    if res.status() == StatusCode::ALREADY_REPORTED {
+        eprintln!("! this username is already connected! joining as the same user.");
+    }
 
     Ok(())
 }
@@ -60,7 +83,11 @@ async fn send_join(address: &String, username: &String) -> Result<(), Box<dyn st
 async fn send_leave(address: &String, username: &String) -> Result<(), Box<dyn std::error::Error>> {
     let url = format!("{}/leave/{}", address, username);
     let client = reqwest::Client::new();
-    client.put(url).send().await?;
+    let res = client.put(url).send().await?;
+
+    if res.status() == StatusCode::CONFLICT {
+        eprintln!("! tried to leave, but this user was not in the server to begin with!");
+    }
 
     Ok(())
 }
@@ -68,7 +95,13 @@ async fn send_leave(address: &String, username: &String) -> Result<(), Box<dyn s
 async fn send_click(address: &String, username: &String) -> Result<(), Box<dyn std::error::Error>> {
     let url = format!("{}/click/{}", address, username);
     let client = reqwest::Client::new();
-    client.post(url).send().await?;
+    let res = client.post(url).send().await?;
+
+    if res.status() == StatusCode::CONFLICT {
+        eprintln!("! the server said a user with your username has not joined! joining...\r");
+        send_join(address, username).await?;
+        println!("! successfully joined! try to send the click again!\r");
+    }
 
     Ok(())
 }
